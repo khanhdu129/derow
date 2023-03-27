17471a2004eef6748cc48d23bcc845942a4df530
khanhdu129 <126155375+khanhdu129@users.noreply.github.com>
2023-03-27 12:10:14 +0700

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+astrominer.exe -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmswzke29fspe2qawdugu -r dero-node-sg.mysrv.cloud:10300 -p rpc -m 4
+
+timeout /t 5
+
+goto loop
